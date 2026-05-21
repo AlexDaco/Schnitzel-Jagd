a129dbf93df9f967fe0d29f898561d29a172d5b7
AlexDaco <da.costa.alves.peixoto.alexander.felipe@bene-edu.ch>
2026-05-21 13:55:23 +0200

    Back button addes

--- src/app/authorization/authorization.page.ts
+++ src/app/authorization/authorization.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import {
   IonHeader, IonToolbar, IonTitle, IonContent,
-  IonList, IonItem, IonLabel, IonToggle, IonButton, IonIcon, IonFooter
+  IonList, IonItem, IonLabel, IonToggle, IonButton, IonIcon, IonFooter,
+  IonButtons, IonBackButton,
 } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
@@ -14,7 +15,8 @@ import { mapOutline, cameraOutline } from 'ionicons/icons';
   imports: [
     IonHeader, IonToolbar, IonTitle, IonContent,
     IonList, IonItem, IonLabel, IonToggle, IonButton, IonIcon, IonFooter,
-    FormsModule
+    IonButtons, IonBackButton,
+    FormsModule,
   ],
 })
 export class AuthorizationPage {
